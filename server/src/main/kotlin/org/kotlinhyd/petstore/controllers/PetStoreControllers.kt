ab68c8fe58d86d8e4c27ca505dcba564e7e49bc2
Chaitanya Chunduri <chaitanyach444@gmail.com>
2020-05-07 17:59:05 +0530

    Added @requestbody

--- server/src/main/kotlin/org/kotlinhyd/petstore/controllers/PetStoreControllers.kt
+++ server/src/main/kotlin/org/kotlinhyd/petstore/controllers/PetStoreControllers.kt
@@ -2,13 +2,14 @@ package org.kotlinhyd.petstore.controllers
 
 import org.kotlinhyd.petstore.dto.PetsDto
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PetStoreControllers {
 
     @PostMapping("/savePet")
-    fun savePet(pet: PetsDto): PetsDto {
+    fun savePet(@RequestBody pet: PetsDto): PetsDto {
         return pet
     }
 }
